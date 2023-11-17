6453f755ffd029b3c2a6c488cca4be22967a18de
alee <alee_zhou@seminstructs.com>
2023-11-17 18:42:51 +0800

    works for telnet protocal

--- src/telnet/telnet_protocal.cpp
+++ src/telnet/telnet_protocal.cpp
@@ -1,22 +1,16 @@
 #include "telnet_protocal.hpp"
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 
 namespace beprotocal {
 
-    Parser::Parser()
-        : stateLine(new StateLine(this))
-        , statePkg(new StatePkg(this))
-    {
-        current = stateLine ;
-    }
-    Parser::~Parser() {
-        delete stateLine ;
-        delete statePkg ;
-    }
-    
-    void Parser::parse(uint8_t * bytes, size_t len){
-        current->parse(bytes,len) ;
+    uint8_t verifysum(uint8_t * data, size_t len, uint8_t base) {
+        for(uint16_t i=0; i<len; i++) {
+            base^= data[i] ;
+        }
+        return base ;
     }
 
     State::State(Parser * parser): parser(parser) {}
@@ -29,17 +23,80 @@ namespace beprotocal {
         printf("StateLine::~StateLine()\n") ;
     }
     void StateLine::parse(uint8_t * bytes, size_t len) {
-        printf("StateLine::parse()\n") ;
+        printf("StateLine::parse()%d\n", len) ;
+        
+        while(len) {
+
+            for(int i=0;i<len;i++) {
+                // 到达行尾
+                if( bytes[i] == '\n' ) {
+                    pkg_t * pkg = new pkg_t() ;
+                    pkg->data_len = pending_len + i+1 ;
+                    pkg->data = malloc(pkg->data_len+1) ;
+                    memcpy(pkg->data, pending, pending_len) ;
+                    memcpy(pkg->data+pending_len, bytes, i+1) ;
+                    pkg->data[pkg->data_len-1] = 0 ;
+
+                    bytes+= i+1 ;
+                    len-= i+1 ;
+                    break ;
+                }
+                // 遇到包头
+                else if( i<len-1 && bytes[i]==parser->H1 && bytes[i+1]==parser->H2 ) {
+
+                }
+                // 遇到断开的包头(H1前一次到达)
+                else if( bytes[i]==parser->H2 && pending_len>0 && pending[pending_len-1]==parser->H1 ){
+
+                }
+            }
+
+
+
+        }
+    }
+    
+    uint8_t StateLine::savePendingData(uint8_t * data, size_t len) {
+        if(pending_len+len > sizeof(pending)-1) {
+            len = sizeof(pending)-1 - pending_len ;
+        }
+        memcpy(pending+pending_len, data, len) ;
+        pending_len+= len ;
     }
     
     // 包数据
-    StatePkg::StatePkg(Parser * parser): State(parser) {
-    }
+    StatePkg::StatePkg(Parser * parser)
+        : State(parser)
+        , pkg(nullptr)
+    {}
     StatePkg::~StatePkg() {
         printf("StatePkg::~StatePkg()\n") ;
     }
     void StatePkg::parse(uint8_t * bytes, size_t len) {
-        printf("StatePkg::parse()\n") ;
+        printf("StatePkg::parse()%d\n", len) ;
+
+
+    }
+    
+    // 上下文类
+    Parser::Parser(uint8_t _H1,uint8_t _H2)
+        : stateLine(new StateLine(this))
+        , statePkg(new StatePkg(this,H1,H2))
+        , H1(_H1), H2(_H2)
+    {
+        current = stateLine ;
+    }
+    Parser::~Parser() {
+        delete stateLine ;
+        delete statePkg ;
+    }
+    
+    void Parser::parse(uint8_t * bytes, size_t len){
+        current->parse(bytes,len) ;
+    }
+    void Parser::setPkgHead(uint8_t _H1,uint8_t _H2) {
+        H1 = _H1 ;
+        H2 = _H2 ;
     }
 }
 
--- src/telnet/telnet_protocal.hpp
+++ src/telnet/telnet_protocal.hpp
@@ -5,6 +5,43 @@
 
 namespace beprotocal {
 
+	enum PkgDef {
+		HEAD1 = 5 ,
+		HEAD2 = 18 ,
+	} ;
+	enum Cmd {
+		RUN = 1					// 执行js代码，无返回值
+		, CALL = 2					// 执行js代码，有返回值
+		, CALL_ASYNC = 3			// 执行js代码，有异步返回值
+		, RSPN = 4					// CMD_CALL 的返回
+		, EXCEPTION = 5				// CMD_RUN/CMD_CALL 的异常
+		, CALLBACK = 6				// 执行本地js代码(远程发起)
+		, OUTPUT = 7				// 远程输出
+		, DATA = 8					// 数据包(用于 CMD_FILE_PULL_REQ 的回应)
+		, MSG = 9					// 主动向客户端推送的事件
+
+		, FILE_OPEN_REQ = 10		// 打开文件，等待后续接受文件后写入. DATA区为文件内容
+		, FILE_OFFSET_REQ = 11		// 偏移
+		, FILE_PUSH_REQ = 12		// 发送文件
+		, FILE_CLOSE_REQ = 13		// 关闭文件
+		, FILE_PULL_REQ = 20		// 拉取文件. DATA区格式：路径(0结尾字符串) + Offset(uint32) + MaxSize(uint16)
+		, RESET = 30				// 重置命令. DATA区格式：boot_level (uint8)
+		, READY = 31				// 系统准备就绪事件
+	} ;
+
+	typedef struct {
+		uint8_t pkgid ;
+		uint8_t cmd ;
+
+		uint8_t * data ;
+		size_t data_len ;
+
+		uint8_t verifysum ;
+	} pkg_t ;
+
+	uint8_t verifysum(uint8_t * data, size_t len, uint8_t base=0) ;
+	
+
     class Parser ;
     class State {
 		protected:
@@ -16,6 +53,10 @@ namespace beprotocal {
     } ;
     
     class StateLine: public State {
+		private:
+			uint8_t pending[256] ;
+			uint8_t pending_len = 0 ;
+			uint8_t savePendingData(uint8_t * data, size_t len) ;
 		public:
 			StateLine(Parser * parser) ;
 			~StateLine() ;
@@ -23,7 +64,12 @@ namespace beprotocal {
     } ;
     
     class StatePkg: public State {
+
+			pkg_t * pkg = nullptr ;
+			size_t pkg_received = 0 ;
+
 		public:
+
 			StatePkg(Parser * parser) ;
 			~StatePkg() ;
 			void parse(uint8_t * bytes, size_t len) ;
@@ -37,13 +83,15 @@ namespace beprotocal {
     class Parser {
 		private:
 			State * current ;
-			State * stateLine ;
-			State * statePkg ;
-			State * statePkgStream ;
+			StateLine * stateLine ;
+			StatePkg * statePkg ;
 		public:
-			Parser() ;
+			uint8_t H1 = HEAD1 ;
+			uint8_t H2 = HEAD2 ;
+			Parser(uint8_t H1=HEAD1,uint8_t H2=HEAD2) ;
 			~Parser() ;
 			void parse(uint8_t * bytes, size_t len) ;
+			void setPkgHead(uint8_t H1=HEAD1,uint8_t H2=HEAD2) ;
 
 		friend class StateLine ;
 		friend class StatePkg ;
