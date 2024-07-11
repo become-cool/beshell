5aba45092ef0e6d4fe9f2a2b9d869939afc7c3e5
alee <alee_zhou@seminstructs.com>
2024-07-11 15:50:37 +0800

    serial.spiX "X"从0开始计数

--- src/module/serial/SPI.hpp
+++ src/module/serial/SPI.hpp
@@ -16,15 +16,15 @@ namespace be {
     private:
         spi_host_device_t busnum ;
 
-        static SPI * spi1 ;
+        static SPI * spi0 ;
         #if SOC_SPI_PERIPH_NUM > 1
-        static SPI * spi2 ;
+        static SPI * spi1 ;
         #endif
         #if SOC_SPI_PERIPH_NUM > 2
-        static SPI * spi3 ;
+        static SPI * spi2 ;
         #endif
         #if SOC_SPI_PERIPH_NUM > 3
-        static SPI * spi4 ;
+        static SPI * spi3 ;
         #endif
 
         std::map<gpio_num_t, spi_device_handle_t> devices ;
