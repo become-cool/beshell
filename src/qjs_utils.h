0a71a8b75ce07cef745e9c74a443cb049eae822f
alee <alee@chou.im>
2024-08-16 21:16:30 +0800

    remote inline

--- src/qjs_utils.h
+++ src/qjs_utils.h
@@ -195,46 +195,11 @@ extern "C" {
         }                                   \
     }
 
-inline void printUncatchException(JSContext *ctx) {
-    JSValue error = JS_GetException(ctx);
-    const char * cstr = JS_ToCString(ctx, error) ;
-    if(cstr) {
-        printf(cstr) ;
-        printf("\n") ;
-        JS_FreeCString(ctx, cstr) ;
+void printUncatchException(JSContext *ctx) ;
 
-        if (JS_IsError(ctx, error)) {
-            JSValue stack = JS_GetPropertyStr(ctx, error, "stack");
-            if (!JS_IsUndefined(stack)) {
-                cstr = JS_ToCString(ctx, stack) ;
-                printf(cstr) ;
-                printf("\n") ;
-                JS_FreeCString(ctx,cstr) ;
-                JS_FreeValue(ctx, stack);
-            }
-        }
-    }
-    JS_FreeValue(ctx,error) ;
-}
+void JSEval(JSContext *ctx, const char * code, int codelen, const char * filename, int eval_flags) ;
 
-inline void JSEval(JSContext *ctx, const char * code, int codelen, const char * filename, int eval_flags) {
-    if(codelen<0) {
-        codelen = strlen(code) ;
-    }
-    JSValue ret = JS_Eval(ctx,code,codelen,filename,eval_flags) ;
-    if( JS_IsException(ret) ) {
-        printUncatchException(ctx) ;
-    }
-    JS_FreeValue(ctx,ret) ;
-}
-
-inline void JSCall(JSContext *ctx, JSValue jsFunc, JSValue jsThis, int argc, JSValueConst * args) {
-    JSValue ret = JS_Call(ctx, jsFunc, jsThis, argc, args) ;
-    if( JS_IsException(ret) ) {
-        printUncatchException(ctx) ;
-    }
-    JS_FreeValue(ctx,ret) ;
-}
+void JSCall(JSContext *ctx, JSValue jsFunc, JSValue jsThis, int argc, JSValueConst * args) ;
 
 #define MALLOC_ARGV1(argv, arg1)                                                    \
     argv = (JSValue*)malloc(sizeof(JSValue)) ;                                      \
