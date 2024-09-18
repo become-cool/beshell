a2c8d4a118523301ff78417e77b1c316eab47a3f
alee <alee@chou.im>
2024-09-18 21:10:39 +0800

    clear debug output

--- src/module/serial/I2S.cpp
+++ src/module/serial/I2S.cpp
@@ -34,7 +34,7 @@ namespace be {
             .communication_format = I2S_COMM_FORMAT_STAND_I2S,
             .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,
             .dma_buf_count = 2,
-            .dma_buf_len = 512,
+            .dma_buf_len = 128,
             .use_apll = true,
             .tx_desc_auto_clear = true,
             .fixed_mclk = 0
@@ -58,8 +58,8 @@ namespace be {
         GET_INT32_PROP_OPT(argv[0], "channel_format", i2s_config.channel_format, I2S_CHANNEL_FMT_RIGHT_LEFT)
         GET_INT32_PROP_OPT(argv[0], "communication_format", i2s_config.communication_format, I2S_COMM_FORMAT_STAND_I2S)
         GET_INT32_PROP_OPT(argv[0], "intr_alloc_flags", i2s_config.intr_alloc_flags, ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM)
-        GET_INT32_PROP_OPT(argv[0], "dma_buf_count", i2s_config.dma_buf_count, 3)
-        GET_INT32_PROP_OPT(argv[0], "dma_buf_len", i2s_config.dma_buf_len, 300)
+        GET_INT32_PROP_OPT(argv[0], "dma_buf_count", i2s_config.dma_buf_count, 2)
+        GET_INT32_PROP_OPT(argv[0], "dma_buf_len", i2s_config.dma_buf_len, 128)
         GET_INT32_PROP_OPT(argv[0], "use_apll", i2s_config.use_apll, 1)
         
         GET_UINT32_PROP(argv[0], "lrclk", pin_config.ws_io_num, )
@@ -69,21 +69,21 @@ namespace be {
 
         // i2s_config.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT ;
         // i2s_config.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT ;
-        dn(I2S_CHANNEL_FMT_RIGHT_LEFT)
-        dn(I2S_CHANNEL_FMT_ONLY_LEFT)
+        // dn(I2S_CHANNEL_FMT_RIGHT_LEFT)
+        // dn(I2S_CHANNEL_FMT_ONLY_LEFT)
 
-        printf("I2S %d, lrclk:%d, sclk:d%, sout:%d\n", pin_config.ws_io_num, pin_config.bck_io_num, pin_config.data_out_num) ;
-        dn(i2s_config.mode)
-        dn(i2s_config.sample_rate)
-        dn(i2s_config.bits_per_sample)
-        dn(i2s_config.channel_format)
-        dn(i2s_config.communication_format)
-        dn(i2s_config.intr_alloc_flags)
-        dn(i2s_config.dma_buf_count)
-        dn(i2s_config.dma_buf_len)
-        dn(i2s_config.use_apll)
-        dn(i2s_config.tx_desc_auto_clear)
-        dn(i2s_config.fixed_mclk)
+        // printf("I2S %d, lrclk:%d, sclk:d%, sout:%d\n", pin_config.ws_io_num, pin_config.bck_io_num, pin_config.data_out_num) ;
+        // dn(i2s_config.mode)
+        // dn(i2s_config.sample_rate)
+        // dn(i2s_config.bits_per_sample)
+        // dn(i2s_config.channel_format)
+        // dn(i2s_config.communication_format)
+        // dn(i2s_config.intr_alloc_flags)
+        // dn(i2s_config.dma_buf_count)
+        // dn(i2s_config.dma_buf_len)
+        // dn(i2s_config.use_apll)
+        // dn(i2s_config.tx_desc_auto_clear)
+        // dn(i2s_config.fixed_mclk)
 
 
         esp_err_t res = i2s_driver_install(that->busnum, &i2s_config, 0, NULL) ;
