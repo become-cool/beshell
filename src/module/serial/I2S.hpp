e2ddbce72e780efcfc5995a34bba5f98f07e90bc
alee <alee@chou.im>
2024-11-27 22:07:22 +0800

    i2s startRecord()/stopRecord()

--- src/module/serial/I2S.hpp
+++ src/module/serial/I2S.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <NativeClass.hpp>
+#include <EventEmitter.hpp>
 #include "driver/i2s.h"
 #include "./soc_serial.h"
 
 namespace be {
-    class I2S: public be::NativeClass {
+    class I2S: public be::EventEmitter {
         DECLARE_NCLASS_META
     private:
         static std::vector<JSCFunctionListEntry> methods ;
@@ -19,12 +19,19 @@ namespace be {
         
         i2s_port_t busnum ;
 
+        TaskHandle_t taskHandle = nullptr ;
+        static void taskRecording(void *arg) ;
+
     public:
         I2S(JSContext * ctx, i2s_port_t busnum) ;
 
         static I2S * flyweight(JSContext *, i2s_port_t) ;
 
         static JSValue setup(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+
+        void onNativeEvent(JSContext *ctx, void * param) ;
+        static JSValue startRecord(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+        static JSValue stopRecord(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
     
     } ;
 }
