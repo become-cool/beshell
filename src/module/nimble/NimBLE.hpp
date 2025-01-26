e9fc5cd8c57c03bbcda0771af4c7c717fc03d1a7
周谦 <aleechou@gmail.com>
2025-01-26 16:49:42 +0800

    nimble scan/connect is ok

--- src/module/nimble/NimBLE.hpp
+++ src/module/nimble/NimBLE.hpp
@@ -3,6 +3,15 @@
 #include "NativeModule.hpp"
 
 namespace be {
+
+    struct js_nimble_event {
+        int type ;
+        union {
+            struct ble_gap_event * raw_event ;
+            struct peer * peer ;
+        } ;
+    } ;
+
     class NimBLE: public be::EventModule {
     public:
         static const char * const name ;
@@ -20,5 +29,14 @@ namespace be {
         static JSValue startScan(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
         static JSValue stopScan(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
         static JSValue isScaning(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+
+        static JSValue connect(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+        static JSValue disconnect(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+        static JSValue discSvc(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+        static JSValue discChar(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+
+        static JSValue read(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+        static JSValue write(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
+        static JSValue subscribe(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) ;
     } ;
 }
