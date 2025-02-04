3e836ebda4019973fb892527e06c37904dd5e4f4
alee <alee201@163.com>
2025-02-04 18:29:58 +0800

    js-to-c.js 接收参数

--- tools/js-to-c.js
+++ tools/js-to-c.js
@@ -1,23 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-const jsDir = __dirname+'/../src/js' // 指定的目录
-
-const files = fs.readdirSync(jsDir)
-
-files.forEach(file => {
-    if (path.extname(file) === '.js') {
-        const jsFilePath = path.join(jsDir, file);
-        const cArrayName = path.basename(file, '.js').replace(/\./g, '_');
-        const cFilePath = path.join(jsDir, `${cArrayName}.c`);
-
-        const data = fs.readFileSync(jsFilePath, 'utf8');
-        fs.writeFileSync(cFilePath, js2c(data, `embeded_js_src_${cArrayName}`), 'utf8');
-
-        console.log(`File ${cFilePath} has been generated`);
-    }
-});
-
+const fs = require('fs')
+const path = require('path')
 
 function js2c (src, cArrayName) {
     let cArray = `static unsigned char ${cArrayName}[] = {\n`;
@@ -68,11 +50,58 @@ ${js2c(src, 'embeded_js_src_'+name)}
     console.log(`Driver class ${namespace}::${name} has been generated`);
 }
 
-const jsDriverDef = [
-    ["PCA9557", "be::driver::io", __dirname+"/../src/js/driver/io/PCA9557.js", __dirname+"/../src/driver/io/PCA9557.cpp"],
-    ["TM1652", "be::driver::io", __dirname+"/../src/js/driver/io/TM1652.js", __dirname+"/../src/driver/io/TM1652.cpp"]
-]
+function genFile(jsFilePath, cFilePath) {
+    const cArrayName = path.basename(jsFilePath, '.js').replace(/\./g, '_')
+    const jsDir = path.dirname(jsFilePath)
+    if(!cFilePath) {
+        cFilePath = path.join(jsDir, `${cArrayName}.c`);
+    }
 
-for(let [name, namespace, jspath, cpppath] of jsDriverDef) {
-    genJSDriver(name, namespace, jspath, cpppath)
+    const data = fs.readFileSync(jsFilePath, 'utf8');
+    fs.writeFileSync(cFilePath, js2c(data, `embeded_js_src_${cArrayName}`), 'utf8');
+
+    console.log(`File ${cFilePath} has been generated`);
 }
+
+function genDir(jsDir) {
+
+    const files = fs.readdirSync(jsDir)
+
+    files.forEach(file => {
+        if (path.extname(file) === '.js') {
+            const jsFilePath = path.join(jsDir, file)
+            genFile(jsFilePath)
+        }
+    });
+}
+
+function genBeShellJS() {
+
+    genDir(__dirname+'/../src/js')
+    genDir(__dirname+'/../src/js/bt')
+
+    const jsDriverDef = [
+        ["PCA9557", "be::driver::io", __dirname+"/../src/js/driver/io/PCA9557.js", __dirname+"/../src/driver/io/PCA9557.cpp"],
+        ["TM1652", "be::driver::io", __dirname+"/../src/js/driver/io/TM1652.js", __dirname+"/../src/driver/io/TM1652.cpp"]
+    ]
+    
+    for(let [name, namespace, jspath, cpppath] of jsDriverDef) {
+        genJSDriver(name, namespace, jspath, cpppath)
+    }
+}
+
+; (function main() {
+
+    if( process.argv.length<=2 ) {
+        genBeShellJS()
+        return
+    }
+    
+    let srcPath = path.resolve(process.cwd(), process.argv[2])
+    let outPath
+    if(process.argv[3]) {
+        outPath = path.resolve(process.cwd(), process.argv[3])
+    }
+    genFile(srcPath, outPath)
+    
+})()
