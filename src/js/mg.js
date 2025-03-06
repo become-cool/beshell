cdf7276459088f496d381996cb8ff496586a952d
alee <alee201@163.com>
2025-03-06 19:52:06 +0800

    fixed: mg.get(), mg.download()

--- src/js/mg.js
+++ src/js/mg.js
@@ -4,19 +4,12 @@ import * as mg from "mg"
 ;[get, download].map(func => exportValue(mg, func.name, func))
 
 
-function WatchDog (ms, timeoutCb){
-  let timer = -1
+function WatchDog (ms, cbTimeout){
+  let timer = setTimeout(cbTimeout,ms)
   function tick() {
     if(timer>-1) {
-      clearTimeout(timer)
+      resetTimeout(timer, ms)
     }
-    timer = setTimeout(()=>{
-      timer = -1
-      if(timer>-1) {
-        clearTimeout()
-      }
-      timeoutCb()
-    },ms)
   }
   tick.stop = ()=>{
     if(timer>-1) {
@@ -24,19 +17,19 @@ function WatchDog (ms, timeoutCb){
     }
     timer = -1
   }
-  tick()
   return tick
 }
 
 function request(url, handle) {
-  let reason=null
   return new Promise((resolve, reject) => {
     let conn = null
     let timeTick = WatchDog(3000,()=>{
       conn && conn.close()
-      reason = "timeout"
+      conn = null
+      reject("timeout")
     })
     conn = mg.connect(url, (event, data) => {
+      if(!conn) return
       switch (event) {
         case 'connect':
           
@@ -56,29 +49,38 @@ function request(url, handle) {
           break
         case 'timeout':
         case 'error':
+        console.log("mg timeout/error",reason)
           reject(reason||event)
           break
         case 'close':
           timeTick.stop()
+          resolve(data);
           break
         case 'http.msg':
         case 'http.chunk':
           timeTick()
-          // 不需要break
+          // 这里没有break
         default:
-          handle(conn,event,data,resolve,reject,reason)
+          handle(conn,event,data)
           break
       }
     })
   })
 }
 
-function get(url) {
-  return request(url, (conn,event,data,resolve,reject)=>{
+async function get(url) {
+  let body = null
+  let received = false
+  await request(url, (conn,event,msg)=>{
     if(event=='http.msg'){
-      resolve(data.body())
+      body = msg.body()
+      received = true
     }
   })
+  if(!received) {
+    throw new Error("the connection was unexpectedly disconnected.")
+  }
+  return body
 }
 
 
@@ -86,12 +88,10 @@ async function download(url, localPath, progress_cb) {
   let fhandle = fs.open(localPath, "w")
   let wroten = 0
   let done = false
-  let total = 0
   try{
-    await request(url, (conn,event,msg,resolve,reject,reason)=>{
+    let t = Date.now()
+    await request(url, (conn,event,msg)=>{
       if(event=='http.msg'||event=='http.chunk'){
-
-        total = msg.bodyLength()
 
         let data = (event == 'http.msg') ? msg.body() : msg.chunk()
         let chunk = data.slice(wroten, data.byteLength)
@@ -103,17 +103,16 @@ async function download(url, localPath, progress_cb) {
         if (wroten == msg.bodyLength()) {
           fs.close(fhandle)
           fhandle = 0
-          resolve()
           done = true
-        }
-      }
-      else if (event=='close'){
-        if(!done) {
-          done = true
-          reject("the connection was unexpectedly disconnected.")
+          conn.close()
         }
       }
     })
+    if(!done) {
+        fs.close(fhandle)
+        fhandle = 0
+        throw new Error("the connection was unexpectedly disconnected.")
+    }
   }catch(e) {
     if(fhandle) {
       fs.close(fhandle)
