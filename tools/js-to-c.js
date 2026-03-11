080b4bc6250ce54443bbf911842c0349d19b10bb
alee <alee201@163.com>
2026-03-11 23:17:41 +0800

    feat: add directory processing for batch conversion of .js files to .c

--- tools/js-to-c.js
+++ tools/js-to-c.js
@@ -75,6 +75,35 @@ function genDir(jsDir) {
     });
 }
 
+function genDirToDist(srcDir, distDir) {
+    // 确保输出目录存在
+    if (!fs.existsSync(distDir)) {
+        fs.mkdirSync(distDir, { recursive: true });
+    }
+
+    const files = fs.readdirSync(srcDir);
+
+    files.forEach(file => {
+        const srcFilePath = path.join(srcDir, file);
+        const stat = fs.statSync(srcFilePath);
+
+        if (stat.isDirectory()) {
+            // 递归处理子目录
+            const subDistDir = path.join(distDir, file);
+            genDirToDist(srcFilePath, subDistDir);
+        } else if (path.extname(file) === '.js') {
+            // 生成对应的 .c 文件
+            const cArrayName = path.basename(file, '.js').replace(/\./g, '_');
+            const cFilePath = path.join(distDir, `${cArrayName}.c`);
+
+            const data = fs.readFileSync(srcFilePath, 'utf8');
+            fs.writeFileSync(cFilePath, js2c(data, `embeded_js_src_${cArrayName}`), 'utf8');
+
+            console.log(`File ${cFilePath} has been generated`);
+        }
+    });
+}
+
 function genBeShellJS() {
 
     genDir(__dirname+'/../src/js')
@@ -102,6 +131,17 @@ function genBeShellJS() {
     if(process.argv[3]) {
         outPath = path.resolve(process.cwd(), process.argv[3])
     }
-    genFile(srcPath, outPath)
+
+    // 如果两个参数都是目录，则批量转换
+    const srcStat = fs.existsSync(srcPath) ? fs.statSync(srcPath) : null;
+    const outStat = outPath && fs.existsSync(outPath) ? fs.statSync(outPath) : null;
+
+    if (srcStat && srcStat.isDirectory() && outPath && (!fs.existsSync(outPath) || outStat && outStat.isDirectory())) {
+        // 两个目录模式：src 目录下的所有 .js 文件输出到 dist 目录
+        genDirToDist(srcPath, outPath);
+    } else {
+        // 单文件模式
+        genFile(srcPath, outPath);
+    }
     
 })()
